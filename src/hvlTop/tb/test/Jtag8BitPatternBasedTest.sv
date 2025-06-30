0403568f81479a930bce0bc26d041ab71a9b80dd
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-06-30 10:48:53 +0530

    Update Jtag8BitPatternBasedTest.sv

--- src/hvlTop/tb/test/Jtag8BitPatternBasedTest.sv
+++ src/hvlTop/tb/test/Jtag8BitPatternBasedTest.sv
@@ -8,7 +8,7 @@ class Jtag8BitPatternBasedTest extends JtagBaseTest;
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual task run_phase (uvm_phase phase);
 
-  JtagControllerDevicePatternBasedVirtualSequence jtagControllerDevicePatternBasedVirtualSequence;
+  JtagVirtualControllerDevicePatternBasedSequence jtagVirtualControllerDevicePatternBasedSequence;
 endclass : Jtag8BitPatternBasedTest
 
 function Jtag8BitPatternBasedTest :: new(string name = "Jtag8BitPatternBasedTest" , uvm_component parent);
@@ -25,12 +25,12 @@ function void Jtag8BitPatternBasedTest :: build_phase(uvm_phase phase);
 endfunction : build_phase
 
 task Jtag8BitPatternBasedTest :: run_phase(uvm_phase phase);
-  jtagControllerDevicePatternBasedVirtualSequence = JtagControllerDevicePatternBasedVirtualSequence :: type_id :: create("JtagControllerDevicePatternBasedVirtualSequence");
-  jtagControllerDevicePatternBasedVirtualSequence.setConfig(jtagEnvConfig.jtagControllerDeviceAgentConfig);
+  jtagVirtualControllerDevicePatternBasedSequence = JtagVirtualControllerDevicePatternBasedSequence :: type_id :: create("jtagVirtualControllerDevicePatternBasedSequence");
+  jtagVirtualControllerDevicePatternBasedSequence.setConfig(jtagEnvConfig.jtagControllerDeviceAgentConfig);
  
   phase.raise_objection(this);
   repeat(NO_OF_TESTS) begin 
-    jtagControllerDevicePatternBasedVirtualSequence.start(jtagEnv.jtagVirtualSequencer);
+    jtagVirtualControllerDevicePatternBasedSequence.start(jtagEnv.jtagVirtualSequencer);
   end 
   phase.drop_objection(this);
 
