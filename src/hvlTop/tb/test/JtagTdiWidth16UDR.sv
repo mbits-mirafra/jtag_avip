c060f5606ba9c5c7e342ce17a8b98a21b5e7f813
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-06-30 10:53:49 +0530

    Update JtagTdiWidth16UDR.sv

--- src/hvlTop/tb/test/JtagTdiWidth16UDR.sv
+++ src/hvlTop/tb/test/JtagTdiWidth16UDR.sv
@@ -26,12 +26,12 @@ function void JtagTdiWidth16UDR :: build_phase(uvm_phase phase);
 endfunction : build_phase
 
 task  JtagTdiWidth16UDR :: run_phase(uvm_phase phase);
-  jtagControllerDeviceTestingVirtualSequence = JtagControllerDeviceTestingVirtualSequence :: type_id :: create("JtagControllerDeviceTestingVirtualSequence");
-  jtagControllerDeviceTestingVirtualSequence.setConfig(jtagEnvConfig.jtagControllerDeviceAgentConfig);
+  jtagVirtualControllerDeviceTestingSequence = JtagVirtualControllerDeviceTestingSequence :: type_id :: create("jtagVirtualControllerDeviceTestingSequence");
+  jtagVirtualControllerDeviceTestingSequence.setConfig(jtagEnvConfig.jtagControllerDeviceAgentConfig);
  
   phase.raise_objection(this);
   repeat( NO_OF_TESTS) begin 
-    jtagControllerDeviceTestingVirtualSequence.start(jtagEnv.jtagVirtualSequencer);
+    jtagVirtualControllerDeviceTestingSequence.start(jtagEnv.jtagVirtualSequencer);
   end 
   phase.drop_objection(this);
 
